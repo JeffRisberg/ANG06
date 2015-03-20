1fe1d423920568202086b8b40fe8c0232f2d6463
Jeff Risberg <jrisberg@aol.com>
2015-03-20 10:18:32 -0700

    organized example like angular example from keenthemes

--- controllers.js
+++ controllers.js
@@ -1,29 +1,17 @@
 myApp.controller('HomeController', ['$scope', function ($scope) {
     $scope.appState = "home";
 
-    $scope.features = [
-        {name: 'Talks', selected: false},
-        {name: 'Listens', selected: false},
-        {name: 'Ties its shoes', selected: false},
-        {name: 'Goes on walks', selected: false},
-        {name: 'Jumps', selected: false}
-    ];
-
     $scope.products = [
-        {name: 'Collie Dog', features: [], selected: false},
-        {name: 'Chimp', features: [], selected: false},
-        {name: 'Parrot', features: [], selected: false},
-        {name: 'Kangaroo', features: [], selected: false}
+        {name: 'Laptop', features: [], selected: false},
+        {name: 'Desktop', features: [], selected: false},
+        {name: 'Monitor', features: [], selected: false},
+        {name: 'Flash drive', features: [], selected: false}
     ];
+}]);
 
-    $scope.deleteProduct = function (product) {
-        $scope.products.splice($scope.products.indexOf(product), 1);
-    };
-
-    $scope.saveProduct = function () {
-        $scope.products.push({name: $scope.addName});
-        $scope.addName = "";
-
-        $scope.appState = "home";
-    };
-}])
+myApp.controller('HeaderController', ['$scope', function ($scope) {
+    $scope.menuItems = [
+        {name: "File"},
+        {name: "TEdit"}
+    ];
+}]);
