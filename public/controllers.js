289343c65911aae23a78cd6a12869a80a666c6ac
Jeff Risberg <jrisberg@aol.com>
2015-04-16 12:52:14 -0700

    merged old ANG06 and movie CRUD example projects

--- public/controllers.js
+++ public/controllers.js
@@ -16,6 +16,47 @@ myApp.controller('ResourceController', function ($scope, Entry) {
     }); // saves an entry, Assuming $scope.entry is the Entry object
 });
 
+angular.module('movieApp.controllers', [])
+    .controller('MovieListController', function ($scope, $state, popupService, $window, Movie) {
+        $scope.movies = Movie.query();
+
+        $scope.deleteMovie = function (movie) {
+            if (popupService.showPopup('Really delete this?')) {
+                movie.$delete(function () {
+                    $window.location.href = '';
+                });
+            }
+        }
+    })
+
+    .controller('MovieViewController', function ($scope, $stateParams, Movie) {
+        $scope.movie = Movie.get({id: $stateParams.id});
+    })
+
+    .controller('MovieCreateController', function ($scope, $state, $stateParams, Movie) {
+        $scope.movie = new Movie();
+
+        $scope.addMovie = function () {
+            $scope.movie.$save(function () {
+                $state.go('movies');
+            });
+        }
+    })
+
+    .controller('MovieEditController', function ($scope, $state, $stateParams, Movie) {
+        $scope.updateMovie = function () {
+            $scope.movie.$update(function () {
+                $state.go('movies');
+            });
+        };
+
+        $scope.loadMovie = function () {
+            $scope.movie = Movie.get({id: $stateParams.id});
+        };
+
+        $scope.loadMovie();
+    });
+
 myApp.controller('HomeController', ['$scope', function ($scope) {
     // nothing here right now
 }]);
