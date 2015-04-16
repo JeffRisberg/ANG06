b3944f22777b2d1d7b87969af3a68ef5860d4fb5
JeffRisberg <jrisberg@aol.com>
2015-04-16 12:40:44 -0700

    integrated movie example templates and ng-resource configuration

--- public/app.js
+++ public/app.js
@@ -1,38 +1,71 @@
 /**
- * Application is called 'products'
+ * Application is called 'movieApp'
  */
 
-var myApp = angular.module('products', ['ui.router','ui.bootstrap'])
-    .config(['$stateProvider', function ($stateProvider) {
-        var home = {
-            name: 'home',
-            url: '/',
-            templateUrl: 'content.html'
-        };
-        var buttons = {
-            name: 'buttons',
-            url: '/buttons',
-            parent: home,
-            templateUrl: 'templates/buttons.html'
-        };
-        var tabs = {
-            name: 'tabs',
-            url: '/tabs',
-            parent: home,
-            templateUrl: 'templates/tabs.html'
-        };
-        var accordion = {
-            name: 'accordion',
-            url: '/accordion',
-            parent: home,
-            templateUrl: 'templates/accordion.html'
-        };
+var myApp = angular.module('movieApp', ['ngResource', 'ui.router', 'ui.bootstrap']);
 
-        $stateProvider.state(home);
-        $stateProvider.state(buttons);
-        $stateProvider.state(tabs);
-        $stateProvider.state(accordion);
-    }])
+angular.module('movieApp.services').factory('Entry', function ($resource) {
+    return $resource('api/entries/:id', { id: '@_id'}, {
+        update: {
+            method: 'PUT'
+        }
+    }, {
+        stripTrailingSlashes: false
+    });
+});
+
+myApp.config(['$stateProvider', function ($stateProvider) {
+
+    var movies = {
+        name: 'movies',
+        url: '/',
+        templateUrl: 'template/movies.html',
+        controller: 'MovieListController'
+    };
+    var viewMovie = {
+        name: 'viewMovie',
+        url: 'movies/:id/view',
+        templateURL: 'templates/movie-view.html',
+        controller: 'MovieViewController'
+    };
+    var newMovie = {
+        name: 'newMovie',
+        url: 'movies/new',
+        templateUrl: 'templates/movie-add.html',
+        controller: 'MovieCreateController'
+    };
+    var editMovie = {
+        movie: 'editMovie',
+        url: 'movies/:id/edit',
+        controller: 'MovieEditController'
+    };
+    var buttons = {
+        name: 'buttons',
+        url: '/buttons',
+        parent: home,
+        templateUrl: 'templates/buttons.html'
+    };
+    var tabs = {
+        name: 'tabs',
+        url: '/tabs',
+        parent: home,
+        templateUrl: 'templates/tabs.html'
+    };
+    var accordion = {
+        name: 'accordion',
+        url: '/accordion',
+        parent: home,
+        templateUrl: 'templates/accordion.html'
+    };
+
+    $stateProvider.state(movies);
+    $stateProvider.state(viewMovie);
+    $stateProvider.state(newMovie);
+    $stateProvider.state(editMovie);
+    $stateProvider.state(buttons);
+    $stateProvider.state(tabs);
+    $stateProvider.state(accordion);
+}])
     .run(['$state', function ($state) {
-        $state.transitionTo('home');
+        $state.transitionTo('movies');
     }]);
